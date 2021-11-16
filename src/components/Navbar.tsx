4e32b6584e52c6179f8a975a8ab1fa7381a7bde6
Brendan Prednis <brendan.prednis@protonmail.com>
2021-11-15 20:25:44 -0800

    added even more flex.
    Signed-off-by: Brendan Prednis <brendan.prednis@protonmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import LoginButton from "./LoginButton";
 // children needs to be a link extension.
 function Navbar() {
     return (
-        <nav className="row-auto flex justify-between px-2 py-4 items-center flex-grow-1">
+        <nav className=" w-full row-auto flex justify-between px-2 py-4 items-center flex-grow-1">
             <div className="brand font-extrabold text-4xl px-2">
                 <Link to="/">Gordon</Link>
             </div>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,20 +4,16 @@ function ProductCard() {
     const renderLoader = () => <p>Loading</p>;
 
     return (
-        <Suspense fallback={renderLoader}>
-            <div className=" sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 px-2 py-2 min-h-12 ">
-                <img src="fakecrystal.jpg" alt="product image of 3d" />
-                <div className="section px-2 py-2  bg-gray-800">
-                    <h2 className="font-bold text-white">product name</h2>
-                    <p className="font-thin text-white">
-                        this is short info text
-                    </p>
-                    <h3 className="text-indigo-200">
-                        this is the price in BGC (BrungusCoin)
-                    </h3>
-                </div>
+        <div className=" w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 px-2 py-2 min-h-12 ">
+            <img src="fakecrystal.jpg" alt="product image of 3d" />
+            <div className="section px-2 py-2  bg-gray-800">
+                <h2 className="font-bold text-white">product name</h2>
+                <p className="font-thin text-white">this is short info text</p>
+                <h3 className="text-indigo-200">
+                    this is the price in BGC (BrungusCoin)
+                </h3>
             </div>
-        </Suspense>
+        </div>
     );
 }
 
