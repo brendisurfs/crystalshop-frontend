32abdb8bf97eddb74c4a1cb2f6d4596de2683412
Brendan Prednis <brendan.prednis@protonmail.com>
2021-11-15 20:07:16 -0800

    addded more flex.
    Signed-off-by: Brendan Prednis <brendan.prednis@protonmail.com>

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 
 function ProductCard() {
+    const renderLoader = () => <p>Loading</p>;
+
     return (
-        <div className=" sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 px-2 py-2 min-h-12 ">
-            <img src="fakecrystal.jpg" alt="product image of 3d" />
-            <div className="section px-2 py-2  bg-gray-800">
-                <h2 className="font-bold text-white">product name</h2>
-                <p className="font-thin text-white">this is short info text</p>
-                <h3 className="text-indigo-200">
-                    this is the price in BGC (BrungusCoin)
-                </h3>
+        <Suspense fallback={renderLoader}>
+            <div className=" sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 px-2 py-2 min-h-12 ">
+                <img src="fakecrystal.jpg" alt="product image of 3d" />
+                <div className="section px-2 py-2  bg-gray-800">
+                    <h2 className="font-bold text-white">product name</h2>
+                    <p className="font-thin text-white">
+                        this is short info text
+                    </p>
+                    <h3 className="text-indigo-200">
+                        this is the price in BGC (BrungusCoin)
+                    </h3>
+                </div>
             </div>
-        </div>
+        </Suspense>
     );
 }
 
--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -2,10 +2,17 @@ import React from "react";
 
 function About() {
     return (
-        <div className=" px-2 py-2 flex-grow">
-            <div className="text-center">
+        <div className=" px-4 py-2 flex-grow">
+            <div className="px-2 flex-col justify-center">
                 <h1 className="text-lg font-bold">About</h1>
-                <p>this is the about page. These are fake crystals.</p>
+                <p>
+                    These are all fake crystals, made through a procedurally
+                    generated pipeline.
+                </p>
+                <p>
+                    It will create a few randomly generated crystals, leave them
+                    up for a few days, and then remove them from the store.
+                </p>
             </div>
         </div>
     );
